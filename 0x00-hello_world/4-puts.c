d1ee5126814619032222e976be7203723ec7f749
DentistOmarAshraf <omar_ashraf@live.com>
2023-06-18 23:57:39 +0000

    nada

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/*describe main*/
 int main(void)
 {
+	/*this function print on stdout*/
 	char myString[] = "\"Programming is like building a multilingual puzzle";
+	/*puts(declared string)*/
 	puts(myString);
-	return 0;
+
+	return (0);
 }
