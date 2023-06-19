b88ad347847560e35fba51bc867fe7bf49b7f8b0
DentistOmarAshraf <omar_ashraf@live.com>
2023-06-19 14:06:42 +0000

    putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descripition: print stdio
+ *
+ * Return :(1)
+ */
+int main(void)
+{
+	/*var declration*/
+	char x = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	/*function putchar*/
+	putchar(ch);
+
+	return (1);
+}
