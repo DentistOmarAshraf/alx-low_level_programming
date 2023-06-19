b3b221d0571e78280401583642a738094dcebb52
DentistOmarAshraf <omar_ashraf@live.com>
2023-06-19 15:41:40 +0000

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,10 +8,6 @@
  */
 int main(void)
 {
-	/*var declration*/
-	char x = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	/*function putchar*/
-	putchar(x);
-
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
