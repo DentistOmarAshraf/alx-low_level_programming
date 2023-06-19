3e712b3b28bc8266f83dd702ef995e120277f328
DentistOmarAshraf <omar_ashraf@live.com>
2023-06-19 15:49:48 +0000

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
  *
  * Descripition: print stdio
  *
- * Return :(1)
+ * Return :Always(1) Failed
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
