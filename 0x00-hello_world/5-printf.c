e2e069db08ce9d7d99da5f1b4d4eba99eb758e4a
DentistOmarAshraf <omar_ashraf@live.com>
2023-06-19 08:48:52 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entrypoint
+ *
+ * Description: printf on stdout stream
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/*printf is function to stdout stream*/
+	return (0);
+	/*always return 0*/
+}
