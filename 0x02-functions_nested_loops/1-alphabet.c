0e21593159159d00b028bd844040b0ac5e0bfebf
DentistOmarAshraf <omar_ashraf@live.com>
2023-06-25 20:32:45 +0000

    alphabet print

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *main - Entry Point
+ *
+ *Description: for loop print alphabet
+ *
+ *Return: Alwyas 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
