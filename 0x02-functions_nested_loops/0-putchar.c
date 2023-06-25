b1e63842b1b60dc6dc30181c38e5a2bfebe29251
DentistOmarAshraf <omar_ashraf@live.com>
2023-06-25 17:10:32 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - Entry Point
+ * main - Entry point
  *
- * Description: 
+ * Description: print(_putchar)
  *
- * Return: Always 0 (Success)
+ * Return: Alwyas 0 (Success)
  */
+
 int main(void)
 {
-	printf("_putchar\n");
-	return(0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
