8f4005ac513b4ac80d770e38db0ba6b28a6cd968
DentistOmarAshraf <omar_ashraf@live.com>
2023-06-26 00:33:53 +0000

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ * @n: any intger
+ *
+ * Description: print last number
+ *
+ * Return: last digit returned
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+
+	_putchar(last + '0');
+
+	return (last);
+}
