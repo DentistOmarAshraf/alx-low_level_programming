ad4ac45d7d70594a69186936bc5e38ecfc314066
DentistOmarAshraf <omar_ashraf@live.com>
2023-06-29 19:41:01 +0000

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,21 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if ((a > b && b > c) || (a > c && c > b))
+	if ((a > b && b > c) || (a > c && c > b) || (a > b && b == c))
 	{
 		largest = a;
 	}
-	else if ((b > a && a > c) || (b > c && c > a))
+	else if ((b > a && a > c) || (b > c && c > a) || (b > a || a == c))
 	{
 		largest = b;
 	}
-	else if ((c > b && b > a) || (c > a && a > b))
+	else if ((c > b && b > a) || (c > a && a > b) || (c > b && a == b))
 	{
 		largest = c;
+	}
+	else if (a == b && b == c)
+	{
+		largest = a;
 	}
 
 	return (largest);
