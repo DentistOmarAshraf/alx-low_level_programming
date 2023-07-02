10b9e2fe512ad5c242766a0a56b1d331d0dbe6a9
DentistOmarAshraf <omar_ashraf@live.com>
2023-07-02 21:42:54 +0000

    date convertor

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,8 +27,8 @@ void print_remaining_days(int month, int day, int year)
 					|| (month == 12 && day > 366))
 			{printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year); }
 			else
-			{printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day); }
+			{printf("Day of the year: %d\n", day - 1);
+			printf("Remaining days: %d\n", 365 - day); }
 		}
 		else
 		{printf("Day of the year: %d\n", day);
