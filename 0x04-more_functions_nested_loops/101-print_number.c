d8031f6ccd4b1d4a002c2ca5f705893425f15531
DentistOmarAshraf <omar_ashraf@live.com>
2023-07-05 01:21:47 +0000

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,41 +7,44 @@
 
 void print_number(int n)
 {
-	if (n >= 0 && n <= 9)
+	int x, y, z, a;
+
+	if (n < 0)
+	{
+		a = n * -1;
+		x = (a % 10);
+		y = (a - x) / 10;
+		_putchar('-');
+		_putchar(y + '0');
+		_putchar(x + '0');
+	}
+	else if (n >= 0 && n <= 9)
 	{
 		_putchar(n + '0');
 	}
-	else
-	if (n > 9 && n <= 99)
+	else if (n > 9 && n <= 99)
 	{
-		int x = n % 10;
-		int y = (n - x) / 10;
-
+		x = n % 10;
+		y = (n - x) / 10;
 		_putchar(y + '0');
 		_putchar(x + '0');
 	}
-	else
-	if (n > 99 && n <= 999)
+	else if (n > 99 && n <= 999)
 	{
-		int x = n % 10;
-		int y = ((n % 100) - x) / 10;
-		int z = (n - (n % 100)) / 100;
-
+		x = n % 10;
+		y = ((n % 100) - x) / 10;
+		z = (n - (n % 100)) / 100;
 		_putchar(z + '0');
 		_putchar(y + '0');
-		_putchar(x + '0');
-	}
-	else
-	if (n > 999 && n <= 9999)
+		_putchar(x + '0'); }
+	else if (n > 999 && n <= 9999)
 	{
-		int x = n % 10;
-		int y = ((n % 100) - x) / 10;
-		int z = ((n - (n % 100) / 100)) % 10;
-		int a = (n - (n % 1000)) / 1000;
-
+		x = n % 10;
+		y = ((n % 100) - x) / 10;
+		z = (n % 1000 - n % 100) / 100;
+		a = (n - (n % 1000)) / 1000;
 		_putchar(a + '0');
 		_putchar(z + '0');
 		_putchar(y + '0');
-		_putchar(x + '0');
-	}
+		_putchar(x + '0'); }
 }
