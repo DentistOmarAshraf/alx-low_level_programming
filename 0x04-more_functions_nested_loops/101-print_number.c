1ba3d8b1095b0e967e6e83e691dc60a089bb9891
DentistOmarAshraf <omar_ashraf@live.com>
2023-07-05 01:59:48 +0000

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,42 +7,34 @@
 
 void print_number(int n)
 {
+	unsigned int i;
 	int x, y, z, a;
 
+	i = n;
+	x = i % 10;
+	y = ((i % 100) - x) / 10;
+	z = (i % 1000 - i % 100) / 100;
+	a = (i - (i % 1000)) / 1000;
+
 	if (n < 0)
-	{
-		a = n * -1;
-		x = (a % 10);
-		y = (a - x) / 10;
 		_putchar('-');
-		_putchar(y + '0');
-		_putchar(x + '0');
-	}
-	else if (n >= 0 && n <= 9)
+
+	if ((n >= 0 && n <= 9) || (n < 0 && n >= -9))
 	{
 		_putchar(n + '0');
 	}
-	else if (n > 9 && n <= 99)
+	else if ((n > 9 && n <= 99) || (n < -9 && n >= -99))
 	{
-		x = n % 10;
-		y = (n - x) / 10;
 		_putchar(y + '0');
 		_putchar(x + '0');
 	}
-	else if (n > 99 && n <= 999)
+	else if ((n > 99 && n <= 999) || (n < -99 && n >= -999))
 	{
-		x = n % 10;
-		y = ((n % 100) - x) / 10;
-		z = (n - (n % 100)) / 100;
 		_putchar(z + '0');
 		_putchar(y + '0');
 		_putchar(x + '0'); }
-	else if (n > 999 && n <= 9999)
+	else if ((n > 999 && n <= 9999) || (n < -999 && n >= -9999))
 	{
-		x = n % 10;
-		y = ((n % 100) - x) / 10;
-		z = (n % 1000 - n % 100) / 100;
-		a = (n - (n % 1000)) / 1000;
 		_putchar(a + '0');
 		_putchar(z + '0');
 		_putchar(y + '0');
