4f34d0ea7b583f53973798896272d1df82a5c7da
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-01 10:54:58 +0000

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * swap_int - swap two values
+ * @a: pointer to integer
+ * @b: pointer to integer
+ * Return: none void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x;
+	int y;
+
+	x = *a;
+	y = *b;
+	*a = y;
+	*b = x;
+}
