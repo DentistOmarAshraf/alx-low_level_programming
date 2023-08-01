556f5ba9c34d4b4620ff178802fcb3d1b4d9a541
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-01 17:40:13 +0000

    print Half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - print half of string array
+ * @str: array
+ * Return: none
+ */
+void puts_half(char *str)
+{
+	int len, i, x;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	i = len / 2;
+	for (x = i ; x < len ; x++)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
