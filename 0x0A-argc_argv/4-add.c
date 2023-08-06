866cb3c0366e980a77aa09bed4f4e47c065097ac
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-06 01:36:09 +0000

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: number of arrgment passed
+ * @argv: array hold arrgments
+ * Return: Alwyas (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 1 ; i < argc ; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
