11bdc4b5e1e5fb2af23b3cb1e36e40de74684311
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-06 01:40:20 +0000

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,13 +10,19 @@
 int main(int argc, char *argv[])
 {
 	int i, sum;
-
-	sum = 0;
-	for (i = 1 ; i < argc ; i++)
+	
+	if(argc < 3)
 	{
-		sum += atoi(argv[i]);
+		printf("Error\n");
 	}
-	printf("%d\n", sum);
-
+	else
+	{
+		sum = 0;
+		for (i = 1 ; i < argc ; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
 	return (0);
 }
