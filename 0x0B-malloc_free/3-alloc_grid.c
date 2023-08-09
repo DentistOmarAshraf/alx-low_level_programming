02bfcfdaca797bc254f8a62d1ed3d9cc079d6719
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-09 23:59:41 +0000

    2d Array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - intiliaze 2d array with value 0
+ * @width: int represent the width of each array in array
+ * @height: int represent the height of arrays
+ * Return: 2d array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ptr;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	ptr = (int **)malloc(height * sizeof(int *));
+
+	for (i = 0 ; i < height ; i++)
+	{
+		ptr[i] = (int *)malloc(width * sizeof(int));
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+		for (j = 0 ; j < width ; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
