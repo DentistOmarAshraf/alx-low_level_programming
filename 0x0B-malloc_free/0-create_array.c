e92540faba4b0bb0cae1c034d57b667559cabf93
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-09 01:25:10 +0000

    create Array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,13 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	ptr = (char *)malloc(sizeof(char) * size);
-	if (ptr == NULL)
+	if (size == 0)
 	{
 		return (NULL);
 	}
 	else
 	{
+		ptr = (char *)malloc(sizeof(char) * size);
 		i = 0;
 		while (i < size)
 		{
