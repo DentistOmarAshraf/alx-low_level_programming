2e2094b9b3663c988ec2ec0562b6b48dadbcc566
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-10 00:12:24 +0000

    2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,8 +26,12 @@ int **alloc_grid(int width, int height)
 		ptr[i] = (int *)malloc(width * sizeof(int));
 		if (ptr[i] == NULL)
 		{
-			free(ptr);
-			return (NULL);
+			for (j = 0 ; j < i ; j++)
+			{	
+				free(ptr[j]);
+				free(ptr);
+				return (NULL);
+			}
 		}
 		for (j = 0 ; j < width ; j++)
 		{
