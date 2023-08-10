d06d74c68dc38fd6864bfe8d2f776dfc30556239
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-10 01:08:10 +0000

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - function to free memo
+ * @grid: pointer of grid of int type
+ * @height: int type
+ * Return: clean up memo
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if(grid == 0 || grid == NULL)
+	{
+		return;
+	}
+
+	for (i = 0 ; i < height ; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
