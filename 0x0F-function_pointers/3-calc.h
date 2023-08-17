4249e8b5a2fdcae495dcf211fc28b8cd3e7cd116
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-17 02:12:25 +0000

    opfunc

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,15 @@
+#ifndef CALC_H
+#define CALC_H
+/**
+ * struct op - struct op
+ *
+ * @op: the operator
+ * @f: the function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+#endif
