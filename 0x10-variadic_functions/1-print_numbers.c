c469ab34ccf192f17767a84958cca039da03f15c
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-18 12:04:37 +0000

    update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,15 +15,14 @@ void print_numbers(const char *separetor, const unsigned int n, ...)
 	unsigned int i;
 	int x;
 
-	va_start(args,n);
+	va_start(args, n);
 	for (i = 0 ; i < n ; i++)
 	{
-		x = va_arg(args,int);
+		x = va_arg(args, int);
 		printf("%d", x);
 		if (i != (n - 1) && separetor != NULL)
 			printf("%s", separetor);
 	}
 	printf("\n");
 	va_end(args);
-	return;
 }
