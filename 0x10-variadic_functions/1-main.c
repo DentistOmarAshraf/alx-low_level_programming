40ae19229c8eee5216ed13da899ce95b4bcc6147
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-18 12:03:25 +0000

    update

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -1,7 +1,10 @@
 #include "variadic_functions.h"
-
+#include <stddef.h>
+#include <stdio.h>
 int main(void)
 {
-	print_numbers(", ", 4, 0, 98, -1024, 402);
+	print_numbers(NULL, 4, 0, 98, -1024, 402);
+	printf("\n");
+	print_numbers("; ",0);
 	return (0);
 }
