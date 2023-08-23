91c1df306ff52ac04405d1459ebb0f248eedc064
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-23 20:16:02 +0000

    _atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,7 +22,7 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			sign *= -1;
 		if (s[i] >= '0' && s[i] <= '9')
-				n = (n * 10) + (s[i] - '0');
+			n = (n * 10) + (s[i] - '0');
 	}
 	return (n * sign);
 }
