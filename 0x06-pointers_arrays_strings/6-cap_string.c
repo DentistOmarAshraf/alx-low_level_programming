d60966d5068ae64a35804327bc15debd1d16a287
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-24 21:50:43 +0000

    capletter

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -31,10 +31,17 @@ char *cap_string(char *str)
 
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z' && (is_sep(str[i - 1])))
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 			str[i] = str[i] - 32;
+		if (str[i] >= 'a' && str[i] <= 'z' && (is_sep(str[i - 1])))
+		{
+			str[i] = str[i] - 32;
+			if (str[i - 1] == '\t')
+				str[i - 1] = ' ';
+		}
 		else
 			str[i] = str[i];
 	}
+	str[i] = '\0';
 	return (str);
 }
--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -5,10 +5,12 @@ int main(void)
 {
 	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
 	char *ptr;
-
+	char str2[] = "hello world! expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char *ptr2;
 	ptr = cap_string(str);
 	printf("%s", ptr);
-	printf("%s", str);
-
+	printf("%s\n\n", str);
+	ptr2 = cap_string(str2);
+	printf("%s",ptr2);
 	return (0);
 }
