0cbdb0214d8d1b3cba1272d41df7bd40d9f21eaa
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-24 21:28:43 +0000

    capFirst

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * is_sep - function to check separetor
@@ -33,11 +32,7 @@ char *cap_string(char *str)
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z' && (is_sep(str[i - 1])))
-		{
 			str[i] = str[i] - 32;
-			if (str[i - 1] == '\t')
-				str[i - 1] = ' ';
-		}
 		else
 			str[i] = str[i];
 	}
