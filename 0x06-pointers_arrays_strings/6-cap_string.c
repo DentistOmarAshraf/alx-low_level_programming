76e428df6c4410aff08ac16068a75c81f0bfb983
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-24 20:58:29 +0000

    capFirstLetter

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_sep - function to check separetor
+ * @c: char
+ * Return: 1 if sep 0 if not
+ */
+
+int is_sep(char c)
+{
+	int i;
+	char *str = " \t\n,;.!?\"(){}";
+
+	for (i = 0; str[i] != '\0' ; i++)
+	{
+		if (c == str[i])
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * cap_string - captilize first letter of words
+ * @s: string array
+ * Return: char *
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z' && (is_sep(str[i - 1])))
+			str[i] = str[i] - 32;
+		else
+			str[i] = str[i];
+	}
+	str[i] = '\0';
+	return (str);
+}
