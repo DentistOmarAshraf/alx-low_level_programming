6962bb3189b8d756862330cd076612ff13f2787d
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-24 15:38:17 +0000

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len, i;
+	int i;
 
-	len = 0;
-	while (dest[len] != '\0')
-		len++;
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 		dest[i] = src[i];
-/*	dest[len] = '\0';*/
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
