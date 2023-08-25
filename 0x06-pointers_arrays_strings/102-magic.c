1162aec6d7f082d5efcbe16089303be22a6f4fcc
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-25 19:20:41 +0000

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -12,7 +12,8 @@ int main(void)
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
-	 * - only one statment
+	 * - you are not allowed to modify p
+	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 3) = 98;
