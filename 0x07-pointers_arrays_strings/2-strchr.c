5a808c0e90c117263ef5487f2b7e3ee82ae42a3c
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-27 16:26:37 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - return first occurance of char c in char *s
+ * @s: pointer to string array
+ * @c: letter
+ * Return: pointer to first occurance or NULL if there is not
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (s[i] == c)
+			break;
+	}
+	return (s + i);
+}
