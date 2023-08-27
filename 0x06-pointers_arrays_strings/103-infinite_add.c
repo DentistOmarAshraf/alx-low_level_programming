680905a03e21581a88b1735ed946069e9625fcf6
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-27 08:53:54 +0000

    infadd

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,7 +8,7 @@
  * Return: array or 0
  */
 
-char * infinite_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int len1, len2, x, y, z, carry, n, len;
 	char temp;
@@ -19,9 +19,9 @@ char * infinite_add(char *n1, char *n2, char *r, int size_r)
 		;
 	if (len1 > size_r || len2 > size_r)
 		return (0);
-	
+
 	carry = 0;
-	for (x = len1 - 1 , y = len2 - 1 , z = 0 ; z < size_r - 1 ; x-- , y-- , z++)
+	for (x = len1 - 1, y = len2 - 1, z = 0 ; z < size_r - 1 ; x--, y--, z++)
 	{
 		n = carry;
 
@@ -29,7 +29,7 @@ char * infinite_add(char *n1, char *n2, char *r, int size_r)
 			n += n1[x] - '0';
 		if (y >= 0)
 			n += n2[y] - '0';
-		
+
 		if (x < 0 && y < 0 && n == 0)
 			break;
 		carry = n / 10;
@@ -38,7 +38,7 @@ char * infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (x >= 0 || y >= 0 || carry != 0)
 		return (0);
 	r[z] = '\0';
-	
+
 	len = z - 1;
 	for (x = 0 ; x < z / 2; x++)
 	{
