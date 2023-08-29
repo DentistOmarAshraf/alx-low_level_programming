780f233af5a3def5c0b7b6a9ee2a7d8461d5b23c
DentistOmarAshraf <omar_ashraf@live.com>
2023-08-29 17:16:08 +0000

    setstr

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - change throw pointer to pointer
+ * @s: pointer to pointer
+ * @to: pointer
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
