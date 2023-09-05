0de8a02019785a8bb3fe5b644210c99a6e135903
DentistOmarAshraf <omar_ashraf@live.com>
2023-09-05 20:19:27 +0000

    allocGrid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * alloc_grid - pointer to pointer allocatiom
+ * @width: width of 2d array
+ * @height: height of 2d array
+ * Return: 2d array (pointer to pointer)
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **new;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	new = malloc(sizeof(**new) * height);
+	if (new == NULL)
+		return (NULL);
+	for (i = 0 ; i < height ; i++)
+	{
+		new[i] = malloc(sizeof(*new) * width);
+		if (new[i] == NULL)
+			return (NULL);
+	}
+
+	for (i = 0 ; i < height ; i++)
+		for (j = 0 ; j < width ; j++)
+			new[i][j] = 0;
+
+	return (new);
+}
