1df656a3a82981e2dacd600a67c5e9a4cb503031
DentistOmarAshraf <omar_ashraf@live.com>
2023-09-07 15:48:11 +0000

    caloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,12 +9,15 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	unsigned int i;
+	unsigned int *ptr;
 
 	if (nmemb == 0 || size == 0)
-		return (NULL);
+		exit(98);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		exit(98);
+	for (i = 0 ; i < size ; i++)
+		ptr[i] = 0;
 	return (ptr);
 }
