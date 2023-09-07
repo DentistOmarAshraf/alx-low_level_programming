180177232364754ef5864099a3d548e14f7a5140
DentistOmarAshraf <omar_ashraf@live.com>
2023-09-07 13:11:26 +0000

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _calloc - memory allocation
+ * @nmemb: int (sizeof data type)
+ * @size: int
+ * Return: void pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		exit(98);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
