5f004c27143158c136007305f53c1cb5b3e3c1ac
DentistOmarAshraf <omar_ashraf@live.com>
2023-09-07 17:37:30 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,17 +13,36 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *temp;
 	char *ptr2 =(char *)ptr;
 
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr2 == NULL)
+	{
+		ptr2 = malloc(new_size);
+		if (ptr2 == NULL)
+		{
+			free(ptr2);
+			return (NULL);
+		}
+	}
+
+	if (ptr2 != NULL && new_size == 0)
+		free(ptr);
+
 	temp = malloc(old_size);
 	if (temp == NULL)
 		return (NULL);
-	for (i = 0 ; i < old_size ; i++)
+
+	for (i = 0 ; i <= old_size ; i++)
 		*(temp + i) = *(ptr2 + i);
+
 	free(ptr);
 	ptr = malloc(new_size);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0 ; i < new_size ; i++)
+
+	for (i = 0 ; i <= old_size ; i++)
 		*(ptr2 + i) = *(temp + i);
+
 	free(temp);
 	return (ptr2);
 }
