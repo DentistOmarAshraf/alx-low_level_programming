4e55f0c8f4bd677fb4bea4f984d60b2e1667b83f
DentistOmarAshraf <omar_ashraf@live.com>
2023-09-12 21:41:27 +0000

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,9 +10,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d == NULL)
-		return;
-	if (name == NULL || age == 0 || owner == NULL)
+	if (d == NULL)
 		return;
 	d->name = name;
 	(*d).age = age;
