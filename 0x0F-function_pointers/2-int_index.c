a4f57dff4e0d835190d92d09822d73268b62427a
DentistOmarAshraf <omar_ashraf@live.com>
2023-09-13 22:42:52 +0000

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array || size || cmp)
+	if (size <= 0)
+		return (-1);
+	if (array || cmp)
 	{
 		for (i = 0 ; i < size ; i++)
 			if (cmp(array[i]))
