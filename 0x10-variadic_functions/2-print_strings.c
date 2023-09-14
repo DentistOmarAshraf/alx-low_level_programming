62185edeba0a56ed9f8de48878a444246f8fea46
DentistOmarAshraf <omar_ashraf@live.com>
2023-09-14 23:38:23 +0000

    print_str

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,28 +3,6 @@
 #include <stdlib.h>
 
 /**
- * _strcpy - str copy
- * @s: pointer to char
- * Return: pointer to char
- */
-char *_strcpy(char *s)
-{
-	int len, i;
-	char *tosend;
-
-	if (!s)
-		return (NULL);
-	for (len = 0 ; s[len] ; len++)
-		;
-	tosend = malloc(sizeof(char) * (len + 1));
-	if (!tosend)
-		return (NULL);
-	for (i = 0 ; i < len ; i++)
-		tosend[i] = s[i];
-	tosend[i] = '\0';
-	return (tosend);
-}
-/**
  * print_strings - print string with separator
  * @separator: poniter to char
  * @n: int
@@ -42,9 +20,8 @@ void print_strings(const char *separator, unsigned int n, ...)
 	{
 		if (i && separator)
 			printf("%s", separator);
-		x = _strcpy(va_arg(args, char *));
+		x = va_arg(args, char *);
 		printf("%s", (x) ? x : "(nil)");
-		free(x);
 	}
 	printf("\n");
 	va_end(args);
