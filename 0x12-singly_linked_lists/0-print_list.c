a71dd29300ad8cc8abeccec34e800b0f8dbb5662
DentistOmarAshraf <omar_ashraf@live.com>
2023-09-23 00:15:49 +0000

    Some mistakes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,22 @@ size_t print_list(const list_t *h)
 {
 	size_t print;
 	list_t *temp;
+	int len;
 
 	temp = (list_t *)h;
 	print = 0;
 
 	while (temp)
 	{
-		printf("[%d] %s\n", temp->len, (temp->str ? temp->str : "(nil)"));
+		if (temp->str == NULL)
+			len = 0;
+		else
+			for (len = 0; temp->str[len] ; len++)
+				;
+		printf("[%d] %s\n",len, (temp->str ? temp->str : "(nil)"));
 		temp = temp->next;
 		print++;
+		len = 0;
 	}
 	return (print);
 }
