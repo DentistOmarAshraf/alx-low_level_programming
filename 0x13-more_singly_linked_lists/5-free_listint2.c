d97df45258c5997cac25e7f061cff8064c40bee4
DentistOmarAshraf <omar_ashraf@live.com>
2023-09-26 02:06:54 +0000

    Idk

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,6 +12,8 @@ void free_listint2(listint_t **head)
 	if (*head && head)
 	{
 		free_listint2((&(*head)->next));
+		if ((*head)->next)
+			free((*head)->next);
 		free(*head);
 		*head = NULL;
 	}
