0c41de1d9cb0cd8b9da82d0956234b665db98c6d
DentistOmarAshraf <omar_ashraf@live.com>
2023-09-26 10:20:56 +0000

    First push Poping list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - function return value of head of list
+ * and choping the head
+ * @head: pointer of pointer to head of linked list
+ * Return: int
+ */
+
+int pop_listint(listint_t **head)
+{
+	int value;
+	listint_t *temp;
+
+	value = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+
+	return (value);
+}
