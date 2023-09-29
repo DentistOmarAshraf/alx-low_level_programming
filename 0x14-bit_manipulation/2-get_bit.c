39e094404d64a4213839034dd984d741ac64cb83
DentistOmarAshraf <omar_ashraf@live.com>
2023-09-29 22:53:58 +0000

    ....

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,7 +15,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (n == 0 && index < 64)
 		return (0);
 	x = n;
-	for (i = 0 ; i < index ; i++)
+	for (i = 0 ; i < index && i < 64 ; i++)
 	{
 		if (!x)
 			return (-1);
