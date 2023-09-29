b95f41a08620e05618e01a82959b0d3d0cd4cc7c
DentistOmarAshraf <omar_ashraf@live.com>
2023-09-29 19:36:05 +0000

    First Push - get bit at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * get_bit - giting the bit at certin index;
+ * @n: unsigned long int
+ * @index: unsigned int
+ * Return: bit (1 or 0) (-1) if error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+	unsigned long int x;
+
+	x = n;
+	for (i = 0 ; i < index ; i++)
+	{
+		if (!(x >> 1))
+			return (-1);
+		x = x >> 1;
+	}
+	return (1 & x);
+}
+
