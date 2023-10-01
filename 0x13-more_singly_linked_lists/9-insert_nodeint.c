4344a62bc4f0a4ae9fb9ec9f5237572be58aff45
DentistOmarAshraf <omar_ashraf@live.com>
2023-10-01 16:10:27 +0000

    If index out of range--> free malloced new inside function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,44 +2,46 @@
 #include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - inserting node at certin index
- * @head: pointer of pointer of listint_t
- * @idx: size_t (index)
- * @n: value to be inserted
- * Return: pointer of linked list
+ * insert_nodeint_at_index - insert node struct at certin index
+ * @head: pointer of pointer of struct list
+ * @idx: index size_t
+ * @n: int
+ * Return: new node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	size_t i;
 	listint_t *ptr;
 	listint_t *new;
-	unsigned int i;
 
-	ptr = *head;
 	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (!new)
 		return (NULL);
 	new->n = n;
-	if (idx == 0)
+	if (!(*head))
 	{
-		new->next = ptr;
+		*head = new;
+		(*head)->next = NULL;
+		return (new);
+	}
+	if (!idx)
+	{
+		new->next = *head;
 		*head = new;
 		return (new);
 	}
-	if (!ptr)
-	{
-		ptr = new;
-		ptr->next = NULL;
-		return (new);
-	}
+	ptr = *head;
 	for (i = 0 ; i < idx - 1 ; i++)
 	{
-		ptr = ptr->next;
-		if (!(ptr))
+		if (!ptr->next)
+		{
+			free(new);
 			return (NULL);
+		}
+		ptr = ptr->next;
 	}
 	new->next = ptr->next;
 	ptr->next = new;
-
 	return (new);
 }
