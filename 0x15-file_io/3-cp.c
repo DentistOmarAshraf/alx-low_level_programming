06d74e011790addf634fb8d5475eae7d472a5f9c
DentistOmarAshraf <omar_ashraf@live.com>
2023-10-05 17:48:41 +0000

    Try agin

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,9 +10,9 @@
 char *get_err(int n)
 {
 	err_s err[] = {
-		{98, "Can't read from file %s\n"},
-		{99, "Can't write to %s\n"},
-		{100, "Can't close fd %d\n"},
+		{98, "Error: Can't read from file %s\n"},
+		{99, "Error: Can't write to %s\n"},
+		{100, "Error: Can't close fd %d\n"},
 		{0, NULL}
 	};
 	int i = 0;
