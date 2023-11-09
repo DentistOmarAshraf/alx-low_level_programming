ab610dad63a28e944761e1620d316f0b47eb55bf
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-09 22:57:55 +0000

    ------

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,14 +28,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, size_t idx, int num)
 	dlistint_t *ptr;
 	size_t i;
 
-	if (idx == list_size(*head) || idx > list_size(*head))
-		return (NULL);
 	if (!idx || !*head)
 	{
 		new = add_dnodeint(head, num);
 		return (new);
 	}
-	if (idx == list_size(*head) - 1)
+	if (idx == list_size(*head) || idx > list_size(*head))
 	{
 		new = add_dnodeint_end(head, num);
 		return (new);
