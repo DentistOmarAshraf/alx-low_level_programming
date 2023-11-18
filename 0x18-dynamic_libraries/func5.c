42bd234f44029603352c3591afb16ea10a10a0ad
DentistOmarAshraf <omar_ashraf@live.com>
2023-11-18 11:43:24 +0000

    1st - python take c func

---
+++ 0x18-dynamic_libraries/func5.c
@@ -0,0 +1,52 @@
+#include "main2.h"
+
+/**
+ * add - function to add
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub  - function to sub
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - function to multiply
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - function to div
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - function to modul
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/main2.h
@@ -0,0 +1,10 @@
+#ifndef MAIN2_H
+#define MAIN2_H
+
+int add(int ,int);
+int sub(int ,int);
+int mul(int ,int);
+int div(int ,int);
+int mod(int ,int);
+
+#endif
