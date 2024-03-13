8527c771debc6df9a1d1425e5dee602b1037c020
DentistOmarAshraf <omar_ashraf@live.com>
2024-03-13 19:50:45 +0000

    1-binary.c

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Binary Search Algorithm
+ * @arr: pointer of array
+ * @size: size_t size of array
+ * @val: value to search in array
+ * Return: return index and -1 if not found
+ */
+
+int binary_search(int *arr, size_t size, int val)
+{
+	size_t l = 0, r = size - 1, i, mid;
+	int gus;
+
+	while (l <= r)
+	{
+		printf("Searching in array: ");
+		for (i = l ; i <= r ; i++)
+		{
+			if (i != l)
+				printf(", ");
+			printf("%d", arr[i]);
+		}
+		printf("\n");
+
+		mid = (l + r) / 2;
+		gus = arr[mid];
+
+		if (gus == val)
+			return (mid);
+		if (gus < val)
+			l = mid + 1;
+		else
+			r = mid - 1;
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,6 @@
 
 /*----Liner Search Header---*/
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
